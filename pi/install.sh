e2ed52b4e7a47ae0f5d05c43c0a9723ffd1b8ead
satrichard <satrichard@penguin>
2019-11-15 00:14:27 -0800

    added shutdown

---
+++ pi/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y libffi-dev
+
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker pi
+echo "deb https://download.docker.com/linux/raspbian/ stretch stable" >> vim /etc/apt/sources.list
+
+
+sudo apt-get upgrade
+
+
+systemctl start docker.service
+
+
+sudo apt install docker-ce docker-compose
---
+++ pi/start_backend.sh
@@ -0,0 +1,11 @@
+
+
+cd /app/pistargaze/
+
+docker-compose -f local.yml build
+
+docker-compose -f local.yml up
+
+
+
+
