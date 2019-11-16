522ae5b4eb970a7d929a12bd51a13c45c3ae415a
satrichard <satrichard@penguin>
2019-11-15 20:38:33 -0800

    updates

--- pi/shutdown-watcher.sh
+++ pi/shutdown-watcher.sh
@@ -2,8 +2,11 @@ touch /var/run/shutdown_signal/signal
 echo "waiting" > /var/run/shutdown_signal/signal
 while inotifywait -e close_write /var/run/shutdown_signal/signal; do 
   signal=$(cat /var/run/shutdown_signal/signal)
-  if [ "$signal" == "true" ]; then 
+  if [ "$signal" == "shutdown" ]; then 
     echo "done" > /var/run/shutdown_signal/signal
     sudo shutdown -h now
+  elif [ "$signal" == "restart" ]; then
+  	echo "done" > /var/run/shutdown_signal/signal
+  	sudo shutdown -r now
   fi
 done
