2ace7ed05fb2c8024d7cb27e9185ff92ab5c16b2
satrichard <satrichard@penguin>
2019-11-15 19:19:06 -0800

    updates

--- app/pistargaze/config/urls.py
+++ app/pistargaze/config/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # User management
     path("users/", include("pistargaze.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-    path('api/', include('pistargaze.api.urls'), namespace='API'),
+    path('api/', include('pistargaze.api.urls', namespace='api')),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
