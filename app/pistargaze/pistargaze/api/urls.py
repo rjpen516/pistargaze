6585351692b953b3db9d545de239ee8533602601
satrichard <satrichard@penguin>
2019-11-15 19:04:31 -0800

    updates

--- app/pistargaze/pistargaze/api/urls.py
+++ app/pistargaze/pistargaze/api/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from . import views
 
 urlpatterns = [
