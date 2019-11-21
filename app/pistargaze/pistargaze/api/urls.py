fb8add59d109049393b5ecc753f08880face73c1
satrichard <satrichard@penguin>
2019-11-20 20:00:27 -0800

    updates

--- app/pistargaze/pistargaze/api/urls.py
+++ app/pistargaze/pistargaze/api/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 	url(r'command/power$', views.UtilsPower.as_view(), name='Command'),
 	url(r'gps$', views.Position.as_view(), name='GPS'),
 	url(r'command/telescope/slew', views.CommandTelescope.as_view(), name='Slew'),
-	url(r'command/telescope/gpssync',view.SyncGPS.as_view(), name='GPSSync')
+	url(r'command/telescope/gpssync',views.SyncGPS.as_view(), name='GPSSync')
 ]
