ab13d6f9823a67e27a0a27604a8576594017fdce
Richard Penshorn <richard.penshorn@aionescu.rjpen516>
2019-12-21 21:40:44 -0800

    fixes for state

--- app/pistargaze/compose/local/videostream/state.sh
+++ app/pistargaze/compose/local/videostream/state.sh
@@ -5,7 +5,7 @@ while inotifywait -e close_write /var/run/videostream/signal; do
   signal=$(cat /var/run/videostream/signal)
   if [ "$signal" == "stream" ]; then 
     echo "done" > /var/run/videostream/signal
-    gphoto2 --capture-movie --stdout | ./ffmpeg -i pipe:0 http://localhost:8090/feed1.ffm & 
+    gphoto2 --capture-movie --stdout | /opt/ffmpeg -i pipe:0 http://localhost:8090/feed1.ffm & 
   elif [ "$signal" == "stopstream" ]; then
   	echo "done" > /var/run/videostream/signal
   	kill -9 `ps aux  | grep gphoto2 | cut -d$' ' -f 8`
