98f5b77e241b8c2f237aa9f4597f507a06a867ca
Richard Penshorn <satrichard@gmail.com>
2020-02-10 18:15:09 -0800

    updates for dark theme

--- pi/install.sh
+++ pi/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y libffi-dev inotify-tools screen gpsd gpsd-clients python-gps
+apt-get install -y libffi-dev inotify-tools screen python-gps
 
 sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
@@ -16,3 +16,8 @@ systemctl start docker.service
 
 
 sudo apt install docker-ce docker-compose
+
+sudo cp /data/pistargaze/pi/gphoto.service /etc/systemd/system/gphoto.service
+sudo cp /data/pistargaze/pi/gphoto.service /etc/systemd/system/gphoto.service
+sudo systemctl enable gphoto
+sudo systemctl start gphoto
