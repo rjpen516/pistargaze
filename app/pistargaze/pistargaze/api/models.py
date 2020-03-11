a69ac4578badf7da0dda9200701d852999d4e083
Richard Penshorn <satrichard@gmail.com>
2020-03-10 21:55:59 -0700

    fixed models

--- app/pistargaze/pistargaze/api/models.py
+++ app/pistargaze/pistargaze/api/models.py
@@ -18,7 +18,7 @@ class Session(models.Model):
 class Photo(models.Model):
 	session = models.ManyToManyField(Session)
 	token = models.TextField()
-	file = models.FilePathField(max_length=400)
+	file = models.TextField()
 	loc_long = models.DecimalField(decimal_places=5, max_digits=10)
 	loc_lat = models.DecimalField(decimal_places=5, max_digits=10)
 	time = models.DateTimeField(auto_now_add=True)
