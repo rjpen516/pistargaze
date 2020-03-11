fa958b382cee1c1c8e91ffc0872442732fb0111e
Richard Penshorn <satrichard@gmail.com>
2020-03-10 22:46:46 -0700

    attempt 113213123123

--- app/pistargaze/pistargaze/api/views.py
+++ app/pistargaze/pistargaze/api/views.py
@@ -213,17 +213,17 @@ class CaptureAnalysis(APIView):
 
 		#in this method we will run our sky finder, and return out a job session id that we can query until the result is done. It will use the latest image in the capture queue 
 		path = ""
-
-		photo = Photo.objects.get(token=request.query_params.get('token'))
-		path = photo.file
-
-			#path = "pistargaze/static/images/img1.jpg"
+		try:
+			photo = Photo.objects.get(token=request.query_params.get('token'))
+			path = photo.file
+		except Excetion:
+			path = os.path.join(settings.ROOT_DIR,"pistargaze/static/images/img1.jpg")
 
 
 		print(path)
 
 
-		image_data = open(os.path.join(settings.ROOT_DIR,path), "rb")
+		image_data = open(path, "rb")
 
 
 		#we gotta auth to our local version of nova 
