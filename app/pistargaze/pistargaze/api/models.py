91bbb8888088598047050fd0def49f0411ff0714
Richard Penshorn <satrichard@gmail.com>
2020-03-10 20:23:10 -0700

    added migraiton

--- app/pistargaze/pistargaze/api/models.py
+++ app/pistargaze/pistargaze/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Session(models.Model):
+	name = models.CharField(max_length=128)
+
+
+
+class Photo(models.Model):
+	session = models.ManyToManyField(Session)
+	file = models.FilePathField()
+	loc_long = models.DecimalField(decimal_places=5, max_digits=10)
+	loc_lat = models.DecimalField(decimal_places=5, max_digits=10)
+	time = models.DateTimeField(auto_now_add=True)
