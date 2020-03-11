bc9a498f4b421591325df28241a4bd289ec01483
Richard Penshorn <satrichard@gmail.com>
2020-03-10 21:50:49 -0700

    added admin

--- app/pistargaze/pistargaze/api/admin.py
+++ app/pistargaze/pistargaze/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Photo, Session, Stars
+
 # Register your models here.
+
+
+admin.site.register(Photo)
+admin.site.register(Session)
+admin.site.register(Stars)
