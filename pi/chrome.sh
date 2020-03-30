54af2912e9c1e6013843beff880c2a78f4557f08
Richard Penshorn <satrichard@gmail.com>
2020-03-29 20:31:00 -0700

    added fixes

--- pi/chrome.sh
+++ pi/chrome.sh
@@ -5,10 +5,19 @@ xset -dpms
 
 unclutter -idle 0.5 -root &
 
-cd /app/pistargaze
-docker-compose -f local.yml up &
-
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://localhost:8000 &
+service docker start
+
+
+/app/start_backend.sh &
+
+sleep 10
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000 &
+
+while true; do
+   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+   sleep 10
+done
